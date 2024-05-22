f98cb9c6c817964f72029b6b4e2874bb3ca18855
SAI_LAKSHMI_NARASIMHA_RAO <sailakshminarasimharao04@gmail.com>
2024-05-23 00:44:44 +0530

    Created Cart and Order Controllers in backend

--- MedWorldProject/BackEnd/controllers/cart.controller.js
+++ MedWorldProject/BackEnd/controllers/cart.controller.js
@@ -1,13 +1,59 @@
-const productModel=require('../models/cart.model')
+const cartModel = require('../models/cart.model');
+const productModel = require('../models/product.model');
 
-exports.addToCart=async (req,res)=>{
-    
+exports.addToCart = async (req, res) => {
+    const productId = req.params.id; 
+    const userId = req.user._id; 
+
+    try {
+        const product = await productModel.findOne({ productId });
+        if (!product) 
+            return res.status(404).json({ message: "Product not found" });
+        const pricePerItem = product.price;
+        const cartItem = await cartModel.create({
+            cartItemID: productId,
+            userId: userId,
+            productName: product.name,
+            quantity: req.body.quantity,
+            price: (Number(req.body.quantity) * Number(pricePerItem)).toString()
+        });
+
+        return res.status(201).json({ message: "Item added to cart successfully", cartItem });
+    } 
+    catch(error) {
+        console.error("Error adding item to cart:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-exports.showCart=async (req,res)=>{
-    
+
+exports.showCart = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const cartItems = await cartModel.find({ userId });
+        return res.status(200).json(cartItems);
+    }
+    catch (error) {
+        console.error("Error fetching cart items:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-exports.deleteCartItem=async (req,res)=>{
-    
+exports.deleteCartItem = async (req, res) => {
+    const userId = req.user.id; 
+    const cartItemId = req.params.id; 
+
+    try {
+        const cartItem = await cartModel.findOneAndDelete({ _id: cartItemId, userId });
+        if (!cartItem) {
+            return res.status(404).json({ message: "Cart item not found" });
+        }
+
+
+        return res.status(200).json({ message: "Cart item deleted successfully" });
+    } 
+    catch (error) {
+        console.error("Error deleting cart item:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
--- MedWorldProject/BackEnd/controllers/order.controller.js
+++ MedWorldProject/BackEnd/controllers/order.controller.js
@@ -1,13 +1,42 @@
-const productModel=require('../models/order.model')
-
+const cartModel = require('../models/cart.model');
+const orderModel=require('../models/order.model')
 exports.getUserProducts=async (req,res)=>{
-    
+    const id=req.params.id;
+    try{
+        const order=await orderModel.findOne({productId:{$eq:id}});
+        if(!order) res.json({"message":"Order not found"})
+        else res.status(200).json(order)
+    }catch(err){
+        res.json({"message":"Error in loading the order details!!"})
+    }
 }
 
 exports.saveProduct=async (req,res)=>{
-    
+    const userId = req.user.id;
+    try {
+        const cartItems = await cartModel.find({ userId });
+        if(!cartItems) res.json({message:"Cart Items Not Found"})
+        else return res.status(200).json(cartItems);
+    }
+    catch (error) {
+        // console.error("Error fetching cart items:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 exports.placeOrder=async (req,res)=>{
-    
+    try{
+        const {orderId,userId,ProductName,quantity,totalPrice,Status,Price}=req.body;
+        const order=orderModel.create({
+            orderId:orderId,
+            userId:userId,
+            ProductName:ProductName,
+            quantity:quantity,
+            totalPrice:totalPrice,
+            Status:Status,
+            Price:Price
+        })
+        res.status(200).json(order);
+    }
+    catch(err){res.status(404).json({message:"Error in adding order!!!"})}
 }
